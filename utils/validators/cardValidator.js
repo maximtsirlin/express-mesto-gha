8ecb44e79cea289b91fa2f99c657d5ba7ea888b9
Maxim Tsirlin <maximtsirlin@gmail.com>
2023-07-27 18:27:50 +0300

    card

--- utils/validators/cardValidator.js
+++ utils/validators/cardValidator.js
@@ -1,30 +1,20 @@
 const { Joi, celebrate } = require('celebrate');
-const { isURL, isID } = require('../constants');
-const BadRequest = require('../../errors/badRequest-error');
+const { isURL } = require('../constants');
 
-const urlRegex = (url) => {
-  if (isURL) {
-    return url;
-  }
-  throw new BadRequest('Некорректный адрес URL');
-};
-
-const idRegex = (id) => {
-  if (isID) {
-    return id;
-  }
-  throw new BadRequest('Передан некорретный id.');
-};
-
-module.exports.validateCreateCard = celebrate({
+const validateCreateCard = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
-    link: Joi.string().required().custom(urlRegex),
+    link: Joi.string().required().pattern(isURL),
   }),
 });
 
-module.exports.validateCardById = celebrate({
+const validateCardById = celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required().custom(idRegex),
+    id: Joi.string().required().hex().length(24),
   }),
 });
+
+module.exports = {
+  validateCreateCard,
+  validateCardById,
+};
