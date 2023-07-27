19c56aa1d9a0094e994127d10cd7b29e172a1582
Maxim Tsirlin <maximtsirlin@gmail.com>
2023-07-27 17:28:03 +0300

    test

--- utils/validators/userValidator.js
+++ utils/validators/userValidator.js
@@ -1,53 +1,46 @@
 const { celebrate, Joi } = require('celebrate');
-const { isURL, isID } = require('../constants');
-const BadRequest = require('../../errors/badRequest-error');
+const { urlRegex } = require('../constants');
 
-const urlRegex = (url) => {
-  if (isURL(url)) {
-    return url;
-  }
-  throw new BadRequest('Некорректный адрес URL');
-};
-
-const idRegex = (id) => {
-  if (isID(id)) {
-    return id;
-  }
-  throw new BadRequest('Передан некорретный id.');
-};
-
-module.exports.validateLogin = celebrate({
+const validateLogin = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 });
 
-module.exports.validateRegister = celebrate({
+const validateRegister = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(urlRegex),
+  }),
+});
+
+const validateUserId = celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().required().hex().length(24),
+  }),
+});
+
+const validateUserInfo = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().custom(urlRegex),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
   }),
 });
 
-module.exports.validateUserInfo = celebrate({
+const validateAvatar = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required(),
-    about: Joi.string().min(2).max(30).required(),
+    avatar: Joi.string().pattern(urlRegex),
   }),
 });
 
-module.exports.validateAvatar = celebrate({
-  body: Joi.object().keys({
-    avatar: Joi.string().required().custom(urlRegex),
-  }),
-});
-
-module.exports.validateUserId = celebrate({
-  params: Joi.object().keys({
-    userId: Joi.string().required().custom(idRegex),
-  }),
-});
+module.exports = {
+  validateLogin,
+  validateRegister,
+  validateUserId,
+  validateUserInfo,
+  validateAvatar,
+};
